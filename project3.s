00032af20949651bd9a476d62aecfa080553580b
Jailynn <72613082+JailynnPrimus@users.noreply.github.com>
2020-12-08 22:21:10 -0500

    Re-building SubC
    Initializing value 1 for comparison, and the output variable. SecondLoop leads into the 0-9 subsection.

--- project3.s
+++ project3.s
@@ -4,11 +4,18 @@
       invalidWrong: .asciiz "NaN"
       output: .asciiz ""
 .text
-      main:
+       main:
            #Getting User's Input
            li $v0, 8
            la $a0, userInput
            li $a1, 1001
            syscall 
 
-      SubC:
+       SubC:
+           addi $t2, $zero, 1 
+           add $v0, $zero, $zero
+       SecondLoop:
+           add $s6, $a0, $zero
+           slti $t1, $s6, 58
+           beq $t1, $t2, Handlerof0to9
+           j UppercaseHandler
